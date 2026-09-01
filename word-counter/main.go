0dc2303a7af8567f2892b85705baf4d437106f8c
Yamil2717 <71434195+Yamil2717@users.noreply.github.com>
2026-09-01 08:07:20 -0500

    feat: testing for word-counter

--- word-counter/main.go
+++ word-counter/main.go
@@ -3,17 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
-func main() {
-	countLines := false
-	if len(os.Args) > 1 && os.Args[1] == "-l" {
-		countLines = true
-	}
-
-	scanner := bufio.NewScanner(os.Stdin)
+func count(input io.Reader, countLines bool) int {
+	scanner := bufio.NewScanner(input)
 	wordCount := 0
 	lineCount := 0
 
@@ -31,8 +27,22 @@ func main() {
 	}
 
 	if countLines {
-		fmt.Printf("Total lines: %d\n", lineCount)
+		return lineCount
+	}
+	return wordCount
+}
+
+func main() {
+	countLines := false
+	if len(os.Args) > 1 && os.Args[1] == "-l" {
+		countLines = true
+	}
+
+	result := count(os.Stdin, countLines)
+
+	if countLines {
+		fmt.Printf("Total lines: %d\n", result)
 	} else {
-		fmt.Printf("Total words: %d\n", wordCount)
+		fmt.Printf("Total words: %d\n", result)
 	}
 }
