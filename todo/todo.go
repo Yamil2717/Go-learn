84700efa6b5d7cac286201575427c5487f61ea30
Yamil2717 <71434195+Yamil2717@users.noreply.github.com>
2026-09-08 23:41:35 -0500

    feat: save and get tasks package

--- todo/todo.go
+++ todo/todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -41,6 +44,39 @@ func (l *List) Delete(index int) error {
 	}
 
 	*l = append((*l)[:index], (*l)[index+1:]...)
+
+	return nil
+}
+
+func (l *List) Save(filename string) error {
+	jsonData, err := json.Marshal(l)
+	if err != nil {
+		return fmt.Errorf("No se pudo convertir la lista a json: %w", err)
+	}
+
+	if err := os.WriteFile(filename, jsonData, 0644); err != nil {
+		return fmt.Errorf("No se pudo guardar el archivo %s: %w", filename, err)
+	}
+
+	return nil
+}
+
+func (l *List) Get(filename string) error {
+	jsonData, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return fmt.Errorf("El archivo %s no existe: %w", filename, err)
+		}
+		return fmt.Errorf("No se pudo leer el archivo %s: %w", filename, err)
+	}
+
+	if len(jsonData) == 0 {
+		return errors.New("El archivo esta vacio, no hay nada que decodificar")
+	}
+
+	if err := json.Unmarshal(jsonData, l); err != nil {
+		return fmt.Errorf("No se pudo decodificar el json del archivo %s: %w", filename, err)
+	}
 
 	return nil
 }
